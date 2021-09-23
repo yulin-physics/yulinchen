04680cdf146a9014da5a112b914ae99ec6acfd57
Yulin Chen <elim2015@live.com>
2021-09-23 20:30:24 +0100

    test

--- BNTA/react/myprofile/my-app/src/about/About.js
+++ BNTA/react/myprofile/my-app/src/about/About.js
@@ -4,7 +4,7 @@ function About(){
     return(
         <div className="about">
             <h3>ABOUT ME</h3>
-            <p>I graduated from Imperial College London with a physics degree.</p>
+            <p>I graduated from Imperial College London with a physics degree...</p>
         
         </div>
     )
